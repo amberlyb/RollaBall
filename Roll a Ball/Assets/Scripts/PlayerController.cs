3a0dede603cdf01a96fa785479debd5cb6a25909
Amberly Brewster <amberlybrewster@Amberlys-iMac.local>
2019-07-08 15:47:41 -0400

    Challenge working

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,28 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
     public Text countText;
+    public Text scoreText;
     public Text winText;
+    public Text livesText;
+    public Text loseText;
 
     private Rigidbody rb;
     private int count;
+    private int score;
+    private int lives;
+    private Vector3 spherePosition;
+    private float xPosition;
+    private float increaseXPosition;
 
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
-        SetCountText ();
+        score = 0;
+        lives = 3;
+        SetAllText ();
         winText.text = "";
+        loseText.text = "";
     }
 
     void FixedUpdate ()
@@ -33,20 +44,37 @@ public class PlayerController : MonoBehaviour {
     }
     void OnTriggerEnter(Collider other) 
     {
-        if (other.gameObject.CompareTag ("Pick Up"))
-        {
+        if (other.gameObject.CompareTag ("Pick Up")) {
             other.gameObject.SetActive (false);
             count = count + 1;
-           SetCountText ();
+            score = score + 1;
+            SetAllText ();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            lives = lives - 1;
+            SetAllText ();
+        }
+        if (lives <= 0)
+        {
+            Destroy(this);
+            loseText.text = "You Lose!";
+        }
+        if (score == 12) {
+            transform.position = new Vector3 (32.50f, transform.position.y, 0.0f);
         }
     }
-
-    void SetCountText ()
+    void SetAllText ()
     {
+         livesText.text = "Lives: " + lives.ToString ();
          countText.text = "Count: " + count.ToString ();
-         if (count >= 12)
+         scoreText.text = "Score: " + score.ToString ();
+         if (score >= 20)
          {
              winText.text = "You Win!";
          }
+        
     }
 }
---
+++ Roll a Ball/Assets/Scripts/WallController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour
+{
+public float min = 0f;
+public float max = 0f;
+
+void Start () 
+{
+    min = transform.position.z;
+    max = transform.position.z+14;
+}
+void Update () 
+{
+    transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time*2, max-min)+min);
+}
+}
